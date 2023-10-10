0d022b68dc571f85b52f0eb06e7b34fd33bde3e6
jack-the-hack <50553957+jack-the-hack@users.noreply.github.com>
2023-10-10 22:54:51 +0000

    cmt

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, request
-#from flask_mobility import Mobility
+from flask import Flask, request, render_template, render_template_string
+import flask_mobility
 
 app = Flask(__name__)
-#Mobility(app)
+
 
 
 @app.route('/')
 def index():
     return 'Hello from Flask!'
-
-
-#app.run(host='0.0.0.0', port=81)
